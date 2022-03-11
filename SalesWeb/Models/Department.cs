33a93311773653e94cab59eb4cbee74441b55800
Bernardo Marambaia <marambaia.bma@gmail.com>
2022-03-10 23:43:59 -0300

    First Model-Controller-View - Department

---
+++ SalesWeb/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace SalesWeb.Models
+{
+	public class Department
+	{
+		public int Id { get; private set; }
+		public string Name { get; private set; }
+
+		public Department(int id, string name)
+		{
+			Id = id;
+			Name = name;
+		}
+	}
+}
